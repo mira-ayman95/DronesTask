ce1900ae03a9ee30992641cd71b7de968928a419
Mira Ayman <miraayman46@gmail.com>
2022-10-08 15:11:05 +0200

    implement upload files API

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import { createConnection } from "typeorm";
 import express from "express";
 import * as path from "path";
+import cors from "cors";
 import { validateEnv } from "./config";
 validateEnv();
 import { dbConnection } from "./config/connection";
 import DroneRouter from "./routers/drones.router";
+import fileUpload from "express-fileupload"
+import * as fs from "fs/promises"
 
 
 createConnection(dbConnection);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public/medication-images')));
+
+
+app.post('/upload', fileUpload(), async function (req: any, res) {
+    const file = req.files.file;
+    const uploadPath = path.join(__dirname, `../public/medication-images/${file.name}`);
+    await fs.writeFile(uploadPath, file.data)
+    const imageUrl = `${process.env.HOST}:${process.env.PORT}/${file.name}`;
+    res.status(201).send({ imageUrl, imagePath: file.name });
+});
 
 app.use('/drone', DroneRouter);
 
